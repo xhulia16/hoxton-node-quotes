207cf8fe66890da35c3d8dc34a7a123f93f6d2b9
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-08-30 15:30:28 +0200

    quotes

--- index.ts
+++ index.ts
@@ -3,8 +3,36 @@ import express from 'express'
 const app=express()
 const port= 5000
 
+const quotes=[
+    {
+        quote: "One cannot think well, love well, sleep well, if one has not dined well.",
+        author: "Virginia Wolf"
+    },
+    {
+        quote:  "Et tu, Brute?",
+        author: "William Shakespeare"
+    }, 
+    {
+        quote: "Three o'clock is always too late or too early for anything you want to do.",
+        author: "Jean-Paul Sartre"
+    },
+    {
+        quote: "Right or wrong, it's very pleasant to break something from time to time.",
+        author: "Fyodor Dostoevsky"
+    },
+    {
+        quote: "Should I kill myself, or have a cup of coffee?",
+        author: "Albert Camus"
+    }
+
+]
+
 app.get("/", (req, res)=>{
 res.send("hello")
+})
+
+app.get("/quotes", (req, res)=>{
+    res.send(quotes)
 })
 
 app.listen(port)
