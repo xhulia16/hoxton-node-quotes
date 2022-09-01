88b7863fc8b5f666392bba621d14bb36cf937787
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-01 21:13:48 +0200

    seperated quotes and authors

---
+++ data.ts
@@ -0,0 +1,68 @@
+export const quotesData = [
+    {
+        id: 1,
+        quote: "One cannot think well, love well, sleep well, if one has not dined well.",
+        authorId: 1
+
+    },
+    {
+        id: 2,
+        quote: "Et tu, Brute?",
+        authorId: 2
+    },
+    {
+        id: 3,
+        quote: "Three o'clock is always too late or too early for anything you want to do.",
+        authorId: 3
+    },
+    {
+        id: 4,
+        quote: "Right or wrong, it's very pleasant to break something from time to time.",
+        authorId: 4
+    },
+    {
+        id: 5,
+        quote: "Should I kill myself, or have a cup of coffee?",
+        authorId: 5
+    }
+
+]
+
+export const authors = [
+    {
+        id: 1,
+        firstName: "Virginia",
+        lastName: "Wolf",
+        age: 59,
+        image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/George_Charles_Beresford_-_Virginia_Woolf_in_1902_-_Restoration.jpg"
+
+    },
+    {
+        id: 2,
+        firstName: "William",
+        lastName: "Shakespeare",
+        age: 52,
+        image: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Shakespeare.jpg"
+    },
+    {
+        id: 3,
+        firstName: "Jean-Paul",
+        lastName: "Sartre",
+        age: 74,
+        image: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Sartre_1967_crop.jpg"
+    },
+    {
+        id: 4,
+        firstName: "Fyodor",
+        lastName: "Dostoevsky",
+        age: 59,
+        image: "https://i.pinimg.com/736x/90/d2/db/90d2db4a942c7b64a37963328fea2be2.jpg"
+    },
+    {
+        id: 5,
+        firstName: "Albert",
+        lastName: "Camus",
+        age: 46,
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Albert_Camus%2C_gagnant_de_prix_Nobel%2C_portrait_en_buste%2C_pos%C3%A9_au_bureau%2C_faisant_face_%C3%A0_gauche%2C_cigarette_de_tabagisme.jpg/220px-Albert_Camus%2C_gagnant_de_prix_Nobel%2C_portrait_en_buste%2C_pos%C3%A9_au_bureau%2C_faisant_face_%C3%A0_gauche%2C_cigarette_de_tabagisme.jpg"
+    }
+]
--- index.ts
+++ index.ts
@@ -1,75 +1,39 @@
 import express from 'express'
 import cors from 'cors'
+import {quotesData, authors} from "./data"
 
+let quotes= quotesData
 const app = express()
 const port = 5000
 app.use(express.json())
 
-let quotes = [
-    {
-        id: 1,
-        quote: "One cannot think well, love well, sleep well, if one has not dined well.",
-        firstName: "Virginia",
-        lastName: "Wolf",
-        age: 59,
-        image: "https://upload.wikimedia.org/wikipedia/commons/0/0b/George_Charles_Beresford_-_Virginia_Woolf_in_1902_-_Restoration.jpg"
 
-    },
-    {
-        id: 2,
-        quote: "Et tu, Brute?",
-        firstName: "William",
-        lastName: "Shakespeare",
-        age: 52,
-        image: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Shakespeare.jpg"
 
-    },
-    {
-        id: 3,
-        quote: "Three o'clock is always too late or too early for anything you want to do.",
-        firstName: "Jean-Paul",
-        lastName: "Sartre",
-        age: 74,
-        image: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Sartre_1967_crop.jpg"
-    },
-    {
-        id: 4,
-        quote: "Right or wrong, it's very pleasant to break something from time to time.",
-        firstName: "Fyodor",
-        lastName: "Dostoevsky",
-        age: 59,
-        image: "https://i.pinimg.com/736x/90/d2/db/90d2db4a942c7b64a37963328fea2be2.jpg"
-    },
-    {
-        id: 5,
-        quote: "Should I kill myself, or have a cup of coffee?",
-        firstName: "Albert",
-        lastName: "Camus",
-        age: 46,
-        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Albert_Camus%2C_gagnant_de_prix_Nobel%2C_portrait_en_buste%2C_pos%C3%A9_au_bureau%2C_faisant_face_%C3%A0_gauche%2C_cigarette_de_tabagisme.jpg/220px-Albert_Camus%2C_gagnant_de_prix_Nobel%2C_portrait_en_buste%2C_pos%C3%A9_au_bureau%2C_faisant_face_%C3%A0_gauche%2C_cigarette_de_tabagisme.jpg"
-    }
-
-]
-
-function randomQuote() {
-    return Math.floor(Math.random() * (quotes.length))
-}
+   
 
 app.use(cors())
 app.get("/", (req, res) => {
     res.send("hello")
 })
 
-app.get("/quotes", (req, res) => {
-    res.send(quotes)
+app.get("/quotes", (req, res)=>{
+let quotesToSend=quotes.map(quote=>{
+    let author=authors.find(item=>item.id===quote.authorId)
+    return{...quote, author}
+})
+res.send(quotesToSend)
+})
+
+app.get("/authors", (req, res)=>{
+    res.send(authors)
 })
 
 app.get("/quotes/:id", (req, res) => {
     const id = Number(req.params.id)
     const singleQuote = quotes.find(item => item.id === id)
-
+    const author=authors.find(item=> item.id===singleQuote?.authorId)
     if (singleQuote) {
-        res.send(singleQuote)
+        res.send({...singleQuote, author})
     }
     else {
         res.status(404).send({ error: "not found!" })
@@ -78,7 +42,11 @@ app.get("/quotes/:id", (req, res) => {
 })
 
 app.get("/random", (req, res) => {
-    res.send(quotes[randomQuote()])
+    let random= Math.floor(Math.random() * (quotes.length))
+    let quoteToSend=quotes[random]
+    console.log(quoteToSend)
+    let author=authors.find(author=> author.id===quoteToSend?.authorId)
+    res.send({...quoteToSend, author})
 })
 
 app.post("/quotes", (req, res) => {
@@ -88,17 +56,21 @@ app.post("/quotes", (req, res) => {
     if (typeof req.body.quote !== "string") {
         errors.push("Please enter a valid title")
     }
-    if (typeof req.body.firstName !== "string") {
-        errors.push("Please enter a valid first name")
-    }
-    if (typeof req.body.lastName !== "string") {
-        errors.push("Please enter a valid last name")
-    }
-    if (typeof req.body.age !== "number") {
-        errors.push("Please enter a valid age")
-    }
-    if (typeof req.body.image !== "string") {
-        errors.push("Please enter a valid image address")
+    // if (typeof req.body.firstName !== "string") {
+    //     errors.push("Please enter a valid first name")
+    // }
+    // if (typeof req.body.lastName !== "string") {
+    //     errors.push("Please enter a valid last name")
+    // }
+    // if (typeof req.body.age !== "number") {
+    //     errors.push("Please enter a valid age")
+    // }
+    // if (typeof req.body.image !== "string") {
+    //     errors.push("Please enter a valid image address")
+    // }
+    
+    if (typeof req.body.authorId !== "number" || req.body.authorId> authors.length || req.body.authorId<0) {
+        errors.push("Please enter a valid author")
     }
 
     console.log(req.body)
@@ -106,10 +78,7 @@ app.post("/quotes", (req, res) => {
         const newQuote = {
             id: quotes[quotes.length - 1].id + 1,
             quote: req.body.quote,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            age: req.body.age,
-            image: req.body.image
+            authorId:req.body.authorId
         }
         quotes.push(newQuote)
         res.send(newQuote)
@@ -120,37 +89,37 @@ app.post("/quotes", (req, res) => {
 
 })
 
-app.delete("/quotes/:id", (req, res) => {
-    let id = Number(req.params.id)
-    console.log(id)
-    quotes = quotes.filter(quote => quote.id !== id)
-    res.send({ message: 'Quote deleted successfully.' })
-})
+// app.delete("/quotes/:id", (req, res) => {
+//     let id = Number(req.params.id)
+//     console.log(id)
+//     quotes = quotes.filter(quote => quote.id !== id)
+//     res.send({ message: 'Quote deleted successfully.' })
+// })
 
-app.patch("/quotes/:id", (req, res) => {
-    let id = Number(req.params.id)
-    let match = quotes.find(quote => quote.id === id)
-    if(match){
+// app.patch("/quotes/:id", (req, res) => {
+//     let id = Number(req.params.id)
+//     let match = quotes.find(quote => quote.id === id)
+//     if(match){
 
-    if (req.body.quote) {
-        match.quote = req.body.quote
-    }
-    if (req.body.firstName) {
-        match.firstName = req.body.firstName
-    }
-    if (req.body.lastName) {
-        match.lastName = req.body.lastName
-    }
-    if (req.body.age) {
-        match.age = req.body.age
-    }
-    if (req.body.image) {
-        match.age = req.body.image
-    }
-    res.send(match)}
-    else {
-        res.status(404).send({ error: 'Quote not found.' })
-      }
-})
+//     if (req.body.quote) {
+//         match.quote = req.body.quote
+//     }
+//     if (req.body.firstName) {
+//         match.firstName = req.body.firstName
+//     }
+//     if (req.body.lastName) {
+//         match.lastName = req.body.lastName
+//     }
+//     if (req.body.age) {
+//         match.age = req.body.age
+//     }
+//     if (req.body.image) {
+//         match.age = req.body.image
+//     }
+//     res.send(match)}
+//     else {
+//         res.status(404).send({ error: 'Quote not found.' })
+//       }
+// })
 
 app.listen(port)
