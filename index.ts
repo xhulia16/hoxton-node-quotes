17db39e404fc559dba1108406bc0a0f83a01c244
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-09-02 01:08:41 +0200

    commit

--- index.ts
+++ index.ts
@@ -1,39 +1,39 @@
 import express from 'express'
 import cors from 'cors'
-import {quotesData, authors} from "./data"
+import { quotesData, authors } from "./data"
 
-let quotes= quotesData
+let quotes = quotesData
 const app = express()
 const port = 5000
 app.use(express.json())
 
 
 
-   
+
 
 app.use(cors())
 app.get("/", (req, res) => {
     res.send("hello")
 })
 
-app.get("/quotes", (req, res)=>{
-let quotesToSend=quotes.map(quote=>{
-    let author=authors.find(item=>item.id===quote.authorId)
-    return{...quote, author}
-})
-res.send(quotesToSend)
+app.get("/quotes", (req, res) => {
+    let quotesToSend = quotes.map(quote => {
+        let author = authors.find(item => item.id === quote.authorId)
+        return { ...quote, author }
+    })
+    res.send(quotesToSend)
 })
 
-app.get("/authors", (req, res)=>{
+app.get("/authors", (req, res) => {
     res.send(authors)
 })
 
 app.get("/quotes/:id", (req, res) => {
     const id = Number(req.params.id)
     const singleQuote = quotes.find(item => item.id === id)
-    const author=authors.find(item=> item.id===singleQuote?.authorId)
+    const author = authors.find(item => item.id === singleQuote?.authorId)
     if (singleQuote) {
-        res.send({...singleQuote, author})
+        res.send({ ...singleQuote, author })
     }
     else {
         res.status(404).send({ error: "not found!" })
@@ -42,11 +42,11 @@ app.get("/quotes/:id", (req, res) => {
 })
 
 app.get("/random", (req, res) => {
-    let random= Math.floor(Math.random() * (quotes.length))
-    let quoteToSend=quotes[random]
+    let random = Math.floor(Math.random() * (quotes.length))
+    let quoteToSend = quotes[random]
     console.log(quoteToSend)
-    let author=authors.find(author=> author.id===quoteToSend?.authorId)
-    res.send({...quoteToSend, author})
+    let author = authors.find(author => author.id === quoteToSend?.authorId)
+    res.send({ ...quoteToSend, author })
 })
 
 app.post("/quotes", (req, res) => {
@@ -56,20 +56,7 @@ app.post("/quotes", (req, res) => {
     if (typeof req.body.quote !== "string") {
         errors.push("Please enter a valid title")
     }
-    // if (typeof req.body.firstName !== "string") {
-    //     errors.push("Please enter a valid first name")
-    // }
-    // if (typeof req.body.lastName !== "string") {
-    //     errors.push("Please enter a valid last name")
-    // }
-    // if (typeof req.body.age !== "number") {
-    //     errors.push("Please enter a valid age")
-    // }
-    // if (typeof req.body.image !== "string") {
-    //     errors.push("Please enter a valid image address")
-    // }
-    
-    if (typeof req.body.authorId !== "number" || req.body.authorId> authors.length || req.body.authorId<0) {
+    if (typeof req.body.authorId !== "number" || req.body.authorId > authors.length || req.body.authorId < 0) {
         errors.push("Please enter a valid author")
     }
 
@@ -78,7 +65,7 @@ app.post("/quotes", (req, res) => {
         const newQuote = {
             id: quotes[quotes.length - 1].id + 1,
             quote: req.body.quote,
-            authorId:req.body.authorId
+            authorId: req.body.authorId
         }
         quotes.push(newQuote)
         res.send(newQuote)
@@ -89,12 +76,48 @@ app.post("/quotes", (req, res) => {
 
 })
 
-// app.delete("/quotes/:id", (req, res) => {
-//     let id = Number(req.params.id)
-//     console.log(id)
-//     quotes = quotes.filter(quote => quote.id !== id)
-//     res.send({ message: 'Quote deleted successfully.' })
-// })
+
+
+app.post("/authors", (req, res) => {
+    let errors: string[] = []
+
+    if (typeof req.body.firstName !== "string") {
+        errors.push("Please enter a valid first name")
+    }
+    if (typeof req.body.lastName !== "string") {
+        errors.push("Please enter a valid last name")
+    }
+    if (typeof req.body.age !== "number") {
+        errors.push("Please enter a valid age")
+    }
+    if (typeof req.body.image !== "string") {
+        errors.push("Please enter a valid image address")
+    }
+    console.log(req)
+    if (errors.length === 0) {
+        const newAuthor = {
+            id: authors[authors.length - 1].id + 1,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            age: req.body.age,
+            image: req.body.image,
+
+        }
+        authors.push(newAuthor)
+        res.send(newAuthor)
+    }
+    else {
+        res.status(400).send({ errors: errors })
+    }
+
+})
+
+app.delete("/quotes/:id", (req, res) => {
+    let id = Number(req.params.id)
+    console.log(id)
+    quotes = quotes.filter(quote => quote.id !== id)
+    res.send({ message: 'Quote deleted successfully.' })
+})
 
 // app.patch("/quotes/:id", (req, res) => {
 //     let id = Number(req.params.id)
